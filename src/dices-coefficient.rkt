f98b4f7b8ea87fb05c0b879542601cab9909f45d
Vinícius Regatieri <viniciusvcr@hotmail.com>
2019-10-13 01:06:45 -0300

    count equal bigrams and tests implemented

--- src/dices-coefficient.rkt
+++ src/dices-coefficient.rkt
@@ -1,10 +1,23 @@
 #lang racket
 
+(define (bigram-equal? bigram1 bigram2 index1 index2 counter)
+  (cond
+    [(>= index1 (length bigram1)) counter]
+    [(>= index2 (length bigram2)) (bigram-equal? bigram1 bigram2 (add1 index1) 0 counter)]
+    [(string=? (list-ref bigram1 index1) (list-ref bigram2 index2)) (bigram-equal? bigram1 (remove (list-ref bigram2 index2) bigram2) index1 (add1 index2) (add1 counter))]
+    [else (bigram-equal? bigram1 bigram2 index1 (add1 index2) counter)]
+  )
+)
+
+(define (count-equals bigram1 bigram2)
+  (bigram-equal? bigram1 bigram2 0 0 0)
+)
+
 (define (find-bigrams str index lst)
   (cond 
-    [(equal? (string-ref str index) #\space) (find-bigrams str (add1 index) lst)]
+    ; [(equal? (string-ref str index) #\space) (find-bigrams str (add1 index) lst)]
     [(> (+ index 2) (string-length str)) lst]
-    [(equal? (string-ref str (add1 index)) #\space) (find-bigrams str (+ index 2) lst)]
+    ; [(equal? (string-ref str (add1 index)) #\space) (find-bigrams str (+ index 2) lst)]
     [else (find-bigrams str (add1 index) (append lst (list (substring str index (+ index 2)))))]
   )
 )
@@ -13,7 +26,11 @@
   (define str1-bigrams (find-bigrams str1 0 (list)))
   (define str2-bigrams (find-bigrams str2 0 (list)))
 
+  (define len-str1-bigrams (length str1-bigrams))
+  (define len-str2-bigrams (length str2-bigrams))
+  (define coefficient (/ (* 2 2) (+ len-str1-bigrams len-str2-bigrams)))
+
   (list str1-bigrams str2-bigrams)
 )
 
-(provide dices-coefficient find-bigrams)
+(provide dices-coefficient find-bigrams count-equals)
--- test/dices-coefficient-test.rkt
+++ test/dices-coefficient-test.rkt
@@ -9,18 +9,59 @@
   (test-suite
     "find-bigrams-test"
     (test-case "COMPLEMENT"
-      (check-equal? (find-bigrams "COMPLEMENT" 0 (list)) (list "CO" "OM" "MP" "PL" "LE" "EM" "ME" "EN" "NT"))
+      (check-equal? (find-bigrams "COMPLEMENT" 0 (list)) '("CO" "OM" "MP" "PL" "LE" "EM" "ME" "EN" "NT"))
     )
+
     (test-case "COMPLIMENT" 
-      (check-equal? (find-bigrams "COMPLIMENT" 0 (list)) (list "CO" "OM" "MP" "PL" "LI" "IM" "ME" "EN" "NT"))
+      (check-equal? (find-bigrams "COMPLIMENT" 0 (list)) '("CO" "OM" "MP" "PL" "LI" "IM" "ME" "EN" "NT"))
     )
+    
     (test-case "BIZARRE"
-      (check-equal? (find-bigrams "BIZARRE" 0 (list)) (list "BI" "IZ" "ZA" "AR" "RR" "RE"))
+      (check-equal? (find-bigrams "BIZARRE" 0 (list)) '("BI" "IZ" "ZA" "AR" "RR" "RE"))
     )
-    (test-case "Lorem ipsum dolor sit amet"
-      (check-equal? (find-bigrams "Lorem ipsum dolor sit amet" 0 (list)) '("Lo" "or" "re" "em" "ip" "ps" "su" "um" "do" "ol" "lo" "or" "si" "it" "am" "me" "et"))
+    
+    (test-case "ALGORITHMS ARE FUN"
+      (check-equal? (find-bigrams "ALGORITHMS ARE FUN" 0 (list)) '("AL" "LG" "GO" "OR" "RI" "IT" "TH" "HM" "MS" "S " " A" "AR" "RE" "E " " F" "FU" "UN"))
+    )
+
+    (test-case "LOGARITHMS ARE NOT"
+      (check-equal? (find-bigrams "LOGARITHMS ARE NOT" 0 (list)) '("LO" "OG" "GA" "AR" "RI" "IT" "TH" "HM" "MS" "S " " A" "AR" "RE" "E " " N" "NO" "OT"))
+    ) 
+  )
+)
+
+(define count-equals-test
+  (test-suite
+    "count-equals-test"
+    (test-case "COMPLEMENT-COMPLIMENT"
+      (define bigrams1 (find-bigrams "COMPLEMENT" 0 (list)))
+      (define bigrams2 (find-bigrams "COMPLIMENT" 0 (list)))
+
+      (check-equal? (count-equals bigrams1 bigrams2) 7)
+    )
+
+    (test-case "BAZAAR-BIZARRE"
+      (define bigrams1 (find-bigrams "BAZAAR" 0 (list)))
+      (define bigrams2 (find-bigrams "BIZARRE" 0 (list)))
+
+      (check-equal? (count-equals bigrams1 bigrams2) 2)
+    )
+
+    (test-case "ALGORITHMS ARE FUN-LOGARITHMS ARE NOT"
+      (define bigrams1 (find-bigrams "ALGORITHMS ARE FUN" 0 (list)))
+      (define bigrams2 (find-bigrams "LOGARITHMS ARE NOT" 0 (list)))
+
+      (check-equal? (count-equals bigrams1 bigrams2) 10)
+    )
+
+    (test-case "ASSISTANCE-ASSISTANCE"
+      (define bigrams1 (find-bigrams "ASSISTANCE" 0 (list)))
+      (define bigrams2 (find-bigrams "ASSISTANCE" 0 (list)))
+
+      (check-equal? (count-equals bigrams1 bigrams2) 9)
     )
   )
 )
 
 (run-tests find-bigrams-test)
+(run-tests count-equals-test)
