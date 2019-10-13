7bfda4dc8277101400c55613d543d141a583f6f9
Vinícius Regatieri <viniciusvcr@hotmail.com>
2019-10-13 00:13:57 -0300

    remove spaces from comparison

--- test/dices-coefficient-test.rkt
+++ test/dices-coefficient-test.rkt
@@ -17,6 +17,9 @@
     (test-case "BIZARRE"
       (check-equal? (find-bigrams "BIZARRE" 0 (list)) (list "BI" "IZ" "ZA" "AR" "RR" "RE"))
     )
+    (test-case "Lorem ipsum dolor sit amet"
+      (check-equal? (find-bigrams "Lorem ipsum dolor sit amet" 0 (list)) '("Lo" "or" "re" "em" "ip" "ps" "su" "um" "do" "ol" "lo" "or" "si" "it" "am" "me" "et"))
+    )
   )
 )
 
