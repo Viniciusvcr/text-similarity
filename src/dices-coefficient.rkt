a52393bb8ea36e777f8786aca5f26c93db808e04
Vinicius Regatieri <viniciusvcr@hotmail.com>
2019-10-25 17:50:07 -0300

    remove casos extras dice

--- src/dices-coefficient.rkt
+++ src/dices-coefficient.rkt
@@ -15,9 +15,7 @@
 
 (define (find-bigrams str index lst)
   (cond 
-    ; [(equal? (string-ref str index) #\space) (find-bigrams str (add1 index) lst)]
     [(> (+ index 2) (string-length str)) lst]
-    ; [(equal? (string-ref str (add1 index)) #\space) (find-bigrams str (+ index 2) lst)]
     [else (find-bigrams str (add1 index) (append lst (list (substring str index (+ index 2)))))]
   )
 )
