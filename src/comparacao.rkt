4d0906fa80c3ef23fd7a5ae9f279e94b55278070
Vinicius Regatieri <viniciusvcr@hotmail.com>
2019-10-28 14:25:37 -0300

    comparação retornando o melhor entre os dois

--- src/comparacao.rkt
+++ src/comparacao.rkt
@@ -1,14 +1,24 @@
 #lang racket
 
 (require math/statistics)
+(define (compare)
+    (define dice-results '(1.00 0.59 0.94 0.59))
+    (define lcs-results '(1.00 0.34 0.43 0.78))
+    (define gold-standard '(1.00 0.00 0.37 0.50))
 
-(define dice-results '(1.00 0.59 0.94 0.59))
-(define lcs-results '(1.00 0.34 0.43 0.78))
-(define gold-standard '(1.00 0.00 0.37 0.50))
+    (define dice-cor (correlation dice-results gold-standard))
+    (define lcs-cor (correlation lcs-results gold-standard))
+    
+    (displayln "Cálculo da correlação")
+    (display "  Dice's coefficient      : ")
+    (displayln dice-cor)
+    (display "  Longest Common Substring: ")
+    (displayln lcs-cor)
 
-(displayln "Cálculo da correlação")
+    (cond
+        [(< lcs-cor dice-cor) (displayln "Dice's Coefficient foi o melhor")]
+        [else (displayln "LCS foi o melhor")]
+    )
+)
 
-(display "  Dice's coefficient      : ")
-(correlation dice-results gold-standard)
-(display "  Longest Common Substring: ")
-(correlation lcs-results gold-standard)
+(compare)
